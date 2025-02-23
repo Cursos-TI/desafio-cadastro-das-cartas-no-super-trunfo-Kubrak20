90d1ec86979bfbd656005e2f3bf58087ef37e985
VICTOR RODRIGUES KUBRAK <vickub2004@gmail.com>
2025-02-23 19:11:36 +0000

    modifird: super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 int main(){
-    int populaçao, pontos_turisicos;
+    int populacao, pontos_turisicos;
     float pib, area;
 
 
     printf("Digite a população: \n");
-    scanf("%d", &populaçao);
+    scanf("%d", &populacao);
 
     printf("Digite 0 PIB:\n");
     scanf("%f", &pib);
@@ -18,9 +18,8 @@ int main(){
     printf("Digite o número de pontos turísticos  \n");
     scanf("%d", &pontos_turisicos); 
     
-    printf("População do estado: %d\n Total de pontos turísticos: %d\n", populaçao, pontos_turisicos);
+    printf("População do estado: %d\n Total de pontos turísticos: %d\n", populacao, pontos_turisicos);
     printf("PIB: %f\n Área: %f", pib, area);
 
     return 0;
-
 }
