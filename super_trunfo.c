5832f987e7898e73ec774f1c68c344e3010c4f15
VICTOR RODRIGUES KUBRAK <vickub2004@gmail.com>
2025-03-02 15:51:52 -0300

    Update super_trunfo.c
    tirei as acentuações

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 int main(){
-    int populaçao, pontos_turisicos;
+    int populacao, pontos_turisicos;
     float pib, area;
     char estado[3];
     char nome_da_cidade[20];
@@ -13,20 +13,20 @@ int main(){
     printf("Digite o nome da cidade: \n");
     scanf("%s", &nome_da_cidade);
 
-    printf("Digite o código da cidade: \n");
+    printf("Digite o codigo da cidade: \n");
     scanf("%s", &codigo_da_cidade);
 
-    printf("Digite a população: \n");
-    scanf("%d", &populaçao);
+    printf("Digite a populacao: \n");
+    scanf("%d", &populacao);
 
     printf("Digite 0 PIB:\n");
     scanf("%f", &pib);
     
 
-    printf("Digite a área: \n");
+    printf("Digite a area: \n");
     scanf("%f", &area);
 
-    printf("Digite o número de pontos turísticos  \n");
+    printf("Digite o numero de pontos turisticos  \n");
     scanf("%d", &pontos_turisicos); 
     
     printf("estado: %s\n Nome da cidade: %s\n Código da cidade: %s\n", estado,nome_da_cidade,codigo_da_cidade);
