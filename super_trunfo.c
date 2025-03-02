e792a4967ad03b2ad27281c062a27e3f2cdb71bc
VICTOR RODRIGUES KUBRAK <vickub2004@gmail.com>
2025-03-02 15:49:09 -0300

    Update super_trunfo.c

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,12 +1,23 @@
 #include<stdio.h>
 
 int main(){
-    int populacao, pontos_turisicos;
+    int populaçao, pontos_turisicos;
     float pib, area;
+    char estado[3];
+    char nome_da_cidade[20];
+    char codigo_da_cidade[3];
 
+    printf("Digite o estado: \n");
+    scanf("%s", &estado); 
+    
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome_da_cidade);
+
+    printf("Digite o código da cidade: \n");
+    scanf("%s", &codigo_da_cidade);
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao);
+    scanf("%d", &populaçao);
 
     printf("Digite 0 PIB:\n");
     scanf("%f", &pib);
@@ -18,8 +29,10 @@ int main(){
     printf("Digite o número de pontos turísticos  \n");
     scanf("%d", &pontos_turisicos); 
     
-    printf("População do estado: %d\n Total de pontos turísticos: %d\n", populacao, pontos_turisicos);
+    printf("estado: %s\n Nome da cidade: %s\n Código da cidade: %s\n", estado,nome_da_cidade,codigo_da_cidade);
+    printf("População do estado: %d\n Total de pontos turísticos: %d\n", populaçao, pontos_turisicos);
     printf("PIB: %f\n Área: %f", pib, area);
 
     return 0;
+
 }
